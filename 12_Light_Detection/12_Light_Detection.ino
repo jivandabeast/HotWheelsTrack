bd81f5fbc15980b05c74a1ae4aa0981950a8e450
jivan <jramjisingh@albany.edu>
2020-11-25 22:28:10 -0500

    Fixing index error

--- 12_Light_Detection/12_Light_Detection.ino
+++ 12_Light_Detection/12_Light_Detection.ino
@@ -11,15 +11,18 @@ const char lightPin[] = {3, 4, 5, 6, 7, 8, 2};
 const char topPho[] = {A0, A1, A2, A3, A4, A5};
 const char botPho[] = {A6, A7, A8, A9, A10, A11};
 
-int topVal[5] = {};
-int botVal[5] = {};
+int topVal[6] = {};
+int botVal[6] = {};
 
-int trigTopVal[5] = {};
-int trigBotVal[5] = {};
+int trigTopVal[6] = {};
+int trigBotVal[6] = {};
 
-unsigned long startTime;
-unsigned long endTime;
-unsigned long elapsedTime;
+unsigned long startTime = 0;
+unsigned long endTime = 0;
+unsigned long elapsedTime = 0;
+
+boolean topTrig = true;
+boolean botTrig = true;
 
 void setup() {
   Serial.begin(9600);
@@ -27,7 +30,7 @@ void setup() {
 
   Serial.println();
   Serial.print("Configuring LED pin outputs: ");
-  for (int n = 0; n < 8; n += 1) {
+  for (int n = 0; n < 7; n += 1) {
     Serial.print(n);
     Serial.print(" ");
     pinMode(lightPin[n], OUTPUT);
@@ -54,19 +57,21 @@ void setup() {
 void loop() {
   int botValue = analogRead(A6);
   int topValue = analogRead(A0);
-  if (botValue <= trigBotVal[0]) {
+  if ((botValue <= trigBotVal[0]) && (botTrig)) {
     digitalWrite(3, HIGH);
-    //endTime = millis();
+    botTrig = false;
+    endTime = millis();
 
-    //elapsedTime = (endTime - startTime);
-    //elapsedTime = (elapsedTime / 1000);
-    //Serial.println(elapsedTime);
+    elapsedTime = (endTime - startTime);
+    elapsedTime = (elapsedTime / 1000);
+    Serial.println(elapsedTime);
 
     Serial.println("Triggered bottom photoresistor");
   }
-  if (topValue <= trigTopVal[0]) {
+  if ((topValue <= trigTopVal[0]) && (topTrig)) {
     digitalWrite(4, HIGH);
-    //startTime = millis();
+    topTrig = false;
+    startTime = millis();
     Serial.println("Triggered top photoresistor");
   }
 }
